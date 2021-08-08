119a5c03274b058f1e38d289eaf5ee4d29f25a69
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-08 12:29:03 -0700

    Update routes.py

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -1,12 +1,8 @@
-from flask import Blueprint
+from quart import Blueprint
 from app.helpers.services import ShortUrlService, FileService
 
 main = Blueprint('main', __name__)
 
 @main.get('/uploads/<filename>')
-def uploads(filename):
-    return FileService.get_by_filename()
-
-@main.get('/url/<token>')
-def short_url(token):
-    return ShortUrlService.get_by_token()
+async def uploads(filename):
+    return await FileService.get_by_filename()
