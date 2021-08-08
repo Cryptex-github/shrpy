b042df5ece017069ed48d4390322a212f983a6fb
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-08 12:11:21 -0700

    Remove url shorten

--- app/helpers/main.py
+++ app/helpers/main.py
@@ -1,27 +1,21 @@
 # standard library imports
 import os
 import secrets
-from contextlib import closing
 from urllib.parse import urlparse
 from functools import cached_property
 from mimetypes import guess_extension
 
 # pip imports
 from magic import from_buffer
-import psycopg2
-from flask import url_for, current_app
+from quart import url_for, current_app
 from werkzeug.datastructures import FileStorage
 from werkzeug.utils import safe_join, secure_filename
 
 # local imports
 from app import config
 from app.helpers.utils import create_hmac_hexdigest
-from app.helpers.discord import FileEmbed, ShortUrlEmbed
+from app.helpers.discord import FileEmbed
 
-conn = psycopg2.connect("user=postgres1 dbname=shrpy password=postgres")
-conn.set_session(autocommit=True)
-with conn.cursor() as cursor:
-    cursor.execute("CREATE TABLE IF NOT EXISTS urls (token VARCHAR(10) NOT NULL PRIMARY KEY, url TEXT NOT NULL)")
 
 class File:
     def __init__(self, file_instance: FileStorage, use_original_filename=True):
@@ -138,97 +132,3 @@ class InvalidFileException(Exception):
         file_instance_type = type(self.file_instance)
         return f'{self.file_instance} ({file_instance_type}) is not an instance of werkzeug.datastructures.FileStorage ({FileStorage})'
 
-class ShortUrl:
-    def __init__(self, url: str):
-        url = ''.join(url.lower().split())
-
-        if not url.startswith(('https://', 'http://')):
-            url = f'https://{url}'
-
-        self.url = url
-
-    @cached_property
-    def token(self) -> str:
-        return secrets.token_urlsafe(config.URL_TOKEN_BYTES)
-
-    @cached_property
-    def hmac(self) -> str:
-        """Returns HMAC hash calculated from token, `flask.current_app.secret_key` is used as secret."""
-        return create_hmac_hexdigest(self.token, current_app.secret_key)
-
-    @cached_property
-    def shortened_url(self) -> str:
-        """Returns the shortened URL using `flask.url_for`."""
-        return url_for('main.short_url', token=self.token, _external=True)
-
-    @cached_property
-    def deletion_url(self) -> str:
-        """Returns deletion URL using `flask.url_for`."""
-        return url_for('api.delete_short_url', hmac_hash=self.hmac, token=self.token, _external=True)
-
-    def is_valid(self) -> bool:
-        """Checks if URL is valid"""
-        parsed = urlparse(self.url)
-
-        # Parsed URL must have at least scheme and netloc (e.g. domain name)
-        try:
-            valid = all([parsed.scheme, parsed.netloc]) and parsed.netloc.split('.')[1]
-        except IndexError:
-            valid = False
-
-        if valid is False:
-            current_app.logger.warning(f'URL {self.url} is invalid')
-
-        return valid
-
-    def add(self):
-        """Inserts the URL and token to database."""
-
-        current_app.logger.info(f'Saving short URL for {self.url} as {self.shortened_url}')
-        current_app.logger.info(f'URLs: {self.shortened_url} - {self.deletion_url}')
-
-        with closing(self.get_cursor()) as cursor:
-            cursor.execute("INSERT INTO urls VALUES (%s, %s)", (
-                self.token,
-                self.url
-            ))
-
-    def embed(self) -> ShortUrlEmbed:
-        """Returns ShorturlEmbed instance for this URL."""
-        return ShortUrlEmbed(
-            content_url=self.shortened_url, 
-            deletion_url=self.deletion_url, 
-            original_url=self.url, 
-            shortened_url=self.shortened_url
-        )
-
-    @classmethod
-    def get_by_token(cls, token: str):
-        """Returns the URL for given token from database."""
-        result = None
-        with closing(cls.get_cursor()) as cursor:
-            row = cursor.execute("SELECT url FROM urls WHERE token = %s", (token,))
-            url_row = row.fetchone()
-            if url_row:
-                result = url_row['url']
-        return result
-
-    @classmethod
-    def delete(cls, token: str) -> bool:
-        """DELETEs URL using given token from database."""
-
-        url = cls.get_by_token(token)
-
-        with closing(cls.get_cursor()) as cursor:
-            execute = cursor.execute("DELETE FROM urls WHERE token = %s", (token,))
-            deleted = execute.rowcount > 0
-
-            if deleted:
-                current_app.logger.info(f'Deleted short URL for {url} using token {token}')
-
-            return deleted
-
-    @staticmethod
-    def get_cursor():
-        cursor = conn.cursor()
-        return cursor
