d0b514e1d703a7ce2abacf530b12b7a65d7829aa
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-08 12:49:53 -0700

    Update routes.py

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -5,14 +5,14 @@ from app.helpers.services import FileService
 api = Blueprint('api', __name__)
 
 @api.get('/sharex/upload')
-def upload_config():
-    return FileService.config()
+async def upload_config():
+    return await FileService.config()
 
 @api.post('/upload')
 @auth_required
-def upload():
+async def upload():
     return await FileService.create()
 
 @api.get('/delete-file/<hmac_hash>/<filename>')
-def delete_file(hmac_hash, filename):
+async def delete_file(hmac_hash, filename):
     return await FileService.delete()
