c8929b6e73b79c2545ca972154820bb60c3dbc54
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-08 12:06:04 -0700

    Mirgrate to flask

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 import logging
 
 # pip imports
-from flask import Flask
+from quart import Quart
 from werkzeug.exceptions import HTTPException
 
 # local imports
@@ -12,7 +12,7 @@ from app.helpers.discord import CustomDiscordWebhook
 discord_webhook = CustomDiscordWebhook()
 
 def create_app():
-    app = Flask(__name__)
+    app = Quart(__name__)
 
     # Setup logging
     handler = logger_handler()
